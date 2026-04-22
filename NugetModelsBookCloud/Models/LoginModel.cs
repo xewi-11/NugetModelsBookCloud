11a181d049bca4d0568bdb1533f669487bad02eb
xewi-11 <javi11coimbra@gmail.com>
2026-04-22 18:38:06 +0200

    up

---
+++ NugetModelsBookCloud/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiBookCloud.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
